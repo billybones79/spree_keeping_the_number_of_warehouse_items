5cfb662a3cc98a744771624529b302acae67650b
alexandre DT <alexandredt79@gmail.com>
2015-09-16 16:42:02 -0400

    CALISSE

--- app/controllers/spree/chaindrive_controller.rb
+++ app/controllers/spree/chaindrive_controller.rb
@@ -45,14 +45,15 @@ module Spree
           begin
           chunk.each do |row|
             variant = Spree::Variant.where(sku: row[:sku]).first
+            puts "qty"+row[:qty]
             if variant && row[:qty].is_a?(Integer)
               location = Spree::StockLocation.where(:default => true).first()
               if location
+
                 location.import_warehouse_item(variant, row[:qty], log)
               else
                 log.message =  "Il n'y a pas de location par défaut, veuillez en sélectionner une."
                 log.save
-                puts log.inspect
                 break
               end
             end
