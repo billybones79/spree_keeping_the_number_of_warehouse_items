f2728a4a22d9f515adfed03cf5b7f11d756bcbc5
alexandre DT <alexandredt79@gmail.com>
2017-11-15 09:48:33 -0500

    no byebug

--- app/workers/chaindrive_worker.rb
+++ app/workers/chaindrive_worker.rb
@@ -1,14 +1,12 @@
 class ChaindriveWorker
   include Sidekiq::Worker
   def self.perform(chunk, log_id)
-    byebug
     log = ImportLog.find(log_id)
 
     ActiveRecord::Base.delay_touching do
 
       ActiveRecord::Base.transaction do
         chunk.each do |row|
-          byebug
           variant = Spree::Variant.where(sku: row[:sku]).first
 
           if variant && !variant.is_master? && row[:qty].is_a?(Integer)
