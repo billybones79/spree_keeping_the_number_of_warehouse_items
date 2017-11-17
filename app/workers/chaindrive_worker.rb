09b3799feac005b924aafdc41254dd7bc4f16386
alexandre DT <alexandredt79@gmail.com>
2017-11-17 13:58:49 -0500

    don't even know

--- app/workers/chaindrive_worker.rb
+++ app/workers/chaindrive_worker.rb
@@ -1,9 +1,9 @@
 class ChaindriveWorker
   include Sidekiq::Worker
-  def perform(chunk, log_id)
+
+  def self.perform(chunk, log_id)
     log = ImportLog.find(log_id)
 
-    ActiveRecord::Base.delay_touching do
 
       ActiveRecord::Base.transaction do
         chunk.each do |row|
@@ -21,6 +21,29 @@ class ChaindriveWorker
             end
           end
         end
+    end
+  end
+
+
+  def perform(chunk, log_id)
+    log = ImportLog.find(log_id)
+
+
+      ActiveRecord::Base.transaction do
+        chunk.each do |row|
+          variant = Spree::Variant.where(sku: row[:sku]).first
+
+          if variant && !variant.is_master? && row[:qty].is_a?(Integer)
+            location = Spree::StockLocation.where(:default => true).first()
+            if location
+
+              location.import_warehouse_item(variant, row[:qty], log)
+            else
+              log.message =  "Il n'y a pas de location par défaut, veuillez en sélectionner une."
+              log.save
+              break
+            end
+          end
       end
     end
   end
