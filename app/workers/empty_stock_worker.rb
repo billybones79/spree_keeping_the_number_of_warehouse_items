b0b2243e48cb2d41500fb1528cefe82ccce22fcf
alexandre DT <alexandredt79@gmail.com>
2018-07-30 10:23:18 -0400

    essaie de fixer la remise a 0 de toute

--- app/workers/empty_stock_worker.rb
+++ app/workers/empty_stock_worker.rb
@@ -7,7 +7,7 @@ class EmptyStockWorker
     ActiveRecord::Base.delay_touching do
 
       ActiveRecord::Base.transaction do
-        Spree::StockLocation.where(:default => true).first().stock_items.where.not(variant_id: Spree::Variant.where(sku: (chunk.map { |el| el["sku"] } )).pluck(&:id)).update_all(warehouse_stock: 0, count_on_hand: 0)
+        Spree::StockLocation.where(:default => true).first().stock_items.where.not(variant_id: Spree::Variant.where(sku: (chunk.map { |el| el["sku"] } )).pluck(:id)).update_all(warehouse_stock: 0, count_on_hand: 0)
       end
 
       chunk.each_slice(300).with_index do |chunk, i|
